556eec0f23e18796a9f6870590d4e63a0f6a4da7
Muhammad Dzaky <mdzaky07@gmail.com>
2022-11-09 11:52:44 +0700

    first commit

---
+++ app/src/main/java/com/dzaky/latihanstorage/InternalActivity.java
@@ -0,0 +1,131 @@
+package com.dzaky.latihanstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class InternalActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String FILENAME = "namafile.txt";
+    Button buatFile, ubahFile, bacaFile, deleteFile;
+    TextView textBaca;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internal);
+
+        buatFile = findViewById(R.id.buttonBuatFile);
+        ubahFile = findViewById(R.id.buttonUbahFile);
+        bacaFile = findViewById(R.id.buttonBacaFile);
+        deleteFile = findViewById(R.id.buttonHapusFile);
+        textBaca = findViewById(R.id.textBaca);
+
+        buatFile.setOnClickListener(this);
+        ubahFile.setOnClickListener(this);
+        bacaFile.setOnClickListener(this);
+        deleteFile.setOnClickListener(this);
+    }
+
+    void buatFile() {
+        String isiFile = "Coba Isi Data File Text";
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream outputStream = null;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, true);
+            outputStream.write(isiFile.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void ubahFile(){
+        String ubah = "Semangat Berjuang Peserta DTS";
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream outputStream = null;
+
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, false);
+            outputStream.write(ubah.getBytes());
+            outputStream.flush();
+            outputStream.close();
+
+            Toast.makeText(this, "file berhasil diubah", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void bacaFile(){
+        File sdcard = getFilesDir();
+        File file = new File(sdcard, FILENAME);
+
+        if(file.exists()){
+            StringBuilder text = new StringBuilder();
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+
+                while (line != null){
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+                Toast.makeText(this, "Membaca File", Toast.LENGTH_SHORT).show();
+            }
+            catch (IOException e){
+                System.out.println("Error "+e.getMessage());
+            }
+            textBaca.setText(text.toString());
+        }
+    }
+
+    void hapusFile() {
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()) {
+            file.exists();
+        }
+    }
+
+    public void jalanPerintah(int id) {
+        switch (id) {
+            case R.id.buttonBuatFile:
+                buatFile();
+                break;
+            case R.id.buttonBacaFile:
+                bacaFile();
+                break;
+            case R.id.buttonUbahFile:
+                ubahFile();
+                break;
+            case R.id.buttonHapusFile:
+                hapusFile();
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        jalanPerintah(v.getId());
+    }
+}
